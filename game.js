e63654f96da35099677f890444812ef55982f04a
Vladislav Mamontov <diabloid113@gmail.com>
2017-11-19 22:09:18 +0200

    commit5: game.us changed according to 0 variant №18 (the asteroid became triangular)

--- game.js
+++ game.js
@@ -636,14 +636,7 @@ Asteroid = function () {
   this.init("asteroid",
             [-10,   0,
               -5,   7,
-              -3,   4,
-               1,  10,
-               5,   4,
-              10,   0,
-               5,  -6,
-               2, -10,
-              -4, -10,
-              -4,  -5]);
+              -3,   4]);
 
   this.visible = true;
   this.scale = 6;
