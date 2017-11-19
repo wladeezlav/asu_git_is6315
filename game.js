7e49d38db56a8d6ef656d1d263d17ffea1edcdef
Vladislav Mamontov <diabloid113@gmail.com>
2017-11-19 23:00:28 +0200

    Revert "commit5: game.us changed according to 0 variant №18 (the asteroid became triangular)"
    This reverts commit e63654f96da35099677f890444812ef55982f04a.

--- game.js
+++ game.js
@@ -636,7 +636,14 @@ Asteroid = function () {
   this.init("asteroid",
             [-10,   0,
               -5,   7,
-              -3,   4]);
+              -3,   4,
+               1,  10,
+               5,   4,
+              10,   0,
+               5,  -6,
+               2, -10,
+              -4, -10,
+              -4,  -5]);
 
   this.visible = true;
   this.scale = 6;
